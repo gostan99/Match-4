815ab1d1c29334f04f93c646df046677e61f950d
DESKTOP-8THUBMS\Gostan <vnps1999@gmail.com>
2022-10-30 22:38:56 +0700

    fix UI scale problem in real device

--- Assets/Scripts/PointsUI.cs
+++ Assets/Scripts/PointsUI.cs
@@ -1,9 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
 
 public class PointsUI : MonoBehaviour
 {
